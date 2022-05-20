2d92c5d665c6ed1d7ea3f770fea6bc273e711cff
KungFuDonkey <sietze.riemersma@gmail.com>
2022-05-20 22:32:40 +0200

    clang-format

--- IPCLib/ipclib_export.h
+++ IPCLib/ipclib_export.h
@@ -5,67 +5,65 @@
 #ifdef WIN32
 
 #ifdef IPCLIB_STATIC_DEFINE
-#  define IPCLIB_EXPORT
-#  define IPCLIB_NO_EXPORT
+#define IPCLIB_EXPORT
+#define IPCLIB_NO_EXPORT
 #else
-#  ifndef IPCLIB_EXPORT
-#    ifdef IPCLib_EXPORTS
+#ifndef IPCLIB_EXPORT
+#ifdef IPCLib_EXPORTS
 /* We are building this library */
-#      define IPCLIB_EXPORT __declspec(dllexport)
-#    else
+#define IPCLIB_EXPORT __declspec(dllexport)
+#else
 /* We are using this library */
-#      define IPCLIB_EXPORT __declspec(dllimport)
-#    endif
-#  endif
-
-#  ifndef IPCLIB_NO_EXPORT
-#    define IPCLIB_NO_EXPORT 
-#  endif
+#define IPCLIB_EXPORT __declspec(dllimport)
+#endif
 #endif
 
-#endif // WIN32
+#ifndef IPCLIB_NO_EXPORT
+#define IPCLIB_NO_EXPORT
+#endif
+#endif
+
+#endif  // WIN32
 
 #ifdef __linux__
 
 #ifdef IPCLIB_STATIC_DEFINE
-#  define IPCLIB_EXPORT
-#  define IPCLIB_NO_EXPORT
+#define IPCLIB_EXPORT
+#define IPCLIB_NO_EXPORT
 #else
-#  ifndef IPCLIB_EXPORT
-#    ifdef IPCLib_EXPORTS
+#ifndef IPCLIB_EXPORT
+#ifdef IPCLib_EXPORTS
 /* We are building this library */
-#      define IPCLIB_EXPORT __attribute__((visibility("default")))
-#    else
+#define IPCLIB_EXPORT __attribute__((visibility("default")))
+#else
 /* We are using this library */
-#      define IPCLIB_EXPORT __attribute__((visibility("default")))
-#    endif
-#  endif
+#define IPCLIB_EXPORT __attribute__((visibility("default")))
+#endif
+#endif
 
-#  ifndef IPCLIB_NO_EXPORT
-#    define IPCLIB_NO_EXPORT __attribute__((visibility("hidden")))
-#  endif
+#ifndef IPCLIB_NO_EXPORT
+#define IPCLIB_NO_EXPORT __attribute__((visibility("hidden")))
+#endif
 #endif
 
 #ifndef IPCLIB_DEPRECATED
-#  define IPCLIB_DEPRECATED __attribute__ ((__deprecated__))
+#define IPCLIB_DEPRECATED __attribute__((__deprecated__))
 #endif
 
-#endif // __linux__
+#endif  // __linux__
 
 #ifndef IPCLIB_DEPRECATED_EXPORT
-#  define IPCLIB_DEPRECATED_EXPORT IPCLIB_EXPORT IPCLIB_DEPRECATED
+#define IPCLIB_DEPRECATED_EXPORT IPCLIB_EXPORT IPCLIB_DEPRECATED
 #endif
 
 #ifndef IPCLIB_DEPRECATED_NO_EXPORT
-#  define IPCLIB_DEPRECATED_NO_EXPORT IPCLIB_NO_EXPORT IPCLIB_DEPRECATED
+#define IPCLIB_DEPRECATED_NO_EXPORT IPCLIB_NO_EXPORT IPCLIB_DEPRECATED
 #endif
 
 #if 0 /* DEFINE_NO_DEPRECATED */
-#  ifndef IPCLIB_NO_DEPRECATED
-#    define IPCLIB_NO_DEPRECATED
-#  endif
+#ifndef IPCLIB_NO_DEPRECATED
+#define IPCLIB_NO_DEPRECATED
+#endif
 #endif
 
 #endif /* IPCLIB_EXPORT_H */
-
-
